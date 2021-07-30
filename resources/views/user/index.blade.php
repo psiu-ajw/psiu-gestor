0357f49023d2ef2b8bce7eee72bcc4597d08336f
Mayllon Veras <mvs@cesar.school>
2021-07-30 20:26:11 -0300

    Feature Usuários adicionada

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        
+        <div class="col-md-8">
+            <div class="card">
+               @if ($users->count())
+               <table class="table-auto">
+                <thead>
+                  <tr>
+                    <th class="p-3">Usuário</th>
+                    <th class="p-3">E-mail</th>
+                    <th class="p-3">Cadastro</th>
+                    <th class="p-3">Ações</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  
+                
+               
+                   @foreach ($users as $user)
+                        @if (Auth::user()->email != $user->email)
+                            <tr>
+                                <td class="p-3"> 
+                                    {{$user->name}}
+                                </td>
+                                <td class="p-3">
+                                    {{$user->email}}
+                                </td>
+                                <td class="p-3">{{\Carbon\Carbon::parse($user->created_at)->format('d/m/Y')}}</td>
+                                <td class="p-3 flex">
+                                    <a href="{{ url('user', ['edit', $user->id])}}"> 
+                                        <span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                                <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                    </a>
+                                    <a href="{{ url('user', ['destroy',$user->id])}}"> 
+                                        <span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                            </svg>
+                                        </span>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endif
+                   @endforeach
+                </tbody>
+            </table>
+               @else
+                   <p>Nenhum usuário cadastrado </p>
+               @endif
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/users', [UserController::class, 'index'])->name('users');
+    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+    Route::get('/user/destroy/{id}', [UserController::class, 'destroy'])->name('destroy');
+    Route::get('register', function () {
+        return view('user.register');
+    });
+    Route::post('register', [UserController::class, 'register'])->name('register');
+    Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('edit');
+    Route::post('user/save', [UserController::class, 'save'])->name('user.save');
+    Route::get('/user/changepassword', function () {return view('user.changepassword');});
+    Route::post('user/changepassword', [UserController::class, 'changepassword'])->name('user.changepassword');
 });
