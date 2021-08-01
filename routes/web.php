575996d35bc80aa762c1ffe47630cca7bd3c1cc0
AlvanirJunior <87345354+AlvanirJunior@users.noreply.github.com>
2021-08-01 15:40:51 -0300

    Revert "Feature/users"

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\CommunityController;
 use App\Http\Controllers\ProjetoController;
 use App\Http\Controllers\TiposProjetoController;
 use App\Models\TiposProjeto;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -19,31 +15,11 @@ use App\Models\TiposProjeto;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
 
 Route::get('/', function () {
-
-    return view('auth.login');
-});
-
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::get('/users', [UserController::class, 'index'])->name('users');
-    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
-    Route::get('/user/destroy/{id}', [UserController::class, 'destroy'])->name('destroy');
-    Route::get('register', function () { return view('user.register'); });
-    Route::post('register', [UserController::class, 'register'])->name('register');
-    Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('edit');
-    Route::post('user/save', [UserController::class, 'save'])->name('user.save');
-    Route::get('/user/changepassword', function () {return view('user.changepassword');});
-    Route::post('user/changepassword', [UserController::class, 'changepassword'])->name('user.changepassword');
-    Route::get('community/create', function () { return view('communities.create'); });
-    Route::post('community/create', [CommunityController::class, 'create'])->name('communities.create');
-    Route::get('communities', [CommunityController::class, 'index'])->name('communities');
-    Route::get('/index', [TiposProjetoController::class,  'index'])->name('index');
-    Route::get('/create', [TiposProjetoController::class,  'create'])->name('typeProject');
-    Route::post('/store', [TiposProjetoController::class,   'store'])->name('create.project');
-});
     return view('home');
 });
-
+//Route::get('/index', [TiposProjetoController::class,  'index'])->name('index');
+Route::get('/index', [TiposProjetoController::class,  'index'])->name('index');
+Route::get('/create', [TiposProjetoController::class,  'create'])->name('typeProject');
+Route::post('/store', [TiposProjetoController::class,   'store'])->name('create.project');
