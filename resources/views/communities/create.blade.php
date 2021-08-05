aef71ea2cec856f8e6828f81d37f63f1f6eb3b2d
Mayllon Veras <mvs@cesar.school>
2021-08-05 07:41:13 -0300

    Correção de cadastro e edição de comunidade

--- resources/views/communities/create.blade.php
+++ resources/views/communities/create.blade.php
@@ -7,14 +7,14 @@
                 <div class="card-header">{{ __('Cadastro de comunidade') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('community/create') }}">
+                    <form method="POST" action="{{ route('community.create') }}">
                         @csrf
-
+                        <input id="id" type="hidden" class="form-control" name="id">
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') inválido @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') inválido @enderror" name="name" required autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -25,47 +25,17 @@
                         </div>
 
                         <div class="form-group row">
-
                             <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Descrição') }}</label>
 
                             <div class="col-md-6">
-                                <textarea id="description" class="form-control @error('description') inválida @enderror" name="description" value="{{ old('description') }}" required>
+                                <textarea id="description" class="form-control @error('description') inválida @enderror" name="description" required>
+                                    
                                 </textarea>
                                 @error('description')
-
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') inválido @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') inválida @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirme a senha') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
                         <div class="form-group row mb-0">
--- resources/views/communities/edit.blade.php
+++ resources/views/communities/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Editar de comunidade') }}</div>
+                <div class="card-header">{{ __('Editar comunidade') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('community.save') }}">
