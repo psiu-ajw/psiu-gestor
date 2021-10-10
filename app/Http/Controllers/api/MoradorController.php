4a2389462c06c2a504d178ee1ee526f266f6c06b
Mayllon Veras <mvs@cesar.school>
2021-10-10 06:43:29 -0300

    Incluido retorno de morador já cadastrado

--- app/Http/Controllers/api/MoradorController.php
+++ app/Http/Controllers/api/MoradorController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Morador;
 use App\Http\Resources\Morador as MoradorResource;
+use App\Models\Projeto;
+use Illuminate\Support\Facades\DB;
 
 class MoradorController extends Controller
 {
@@ -16,37 +18,26 @@ class MoradorController extends Controller
 
     public function store(Request $request){
 
-        
+        $morador = new Morador();
         
         if(Morador::where('cpf', sha1($request->input('cpf')))->exists()){
-            return response()->json(["message" => "CPF já cadastrado."], 404);
-        }
-        
-        
-        $morador = new Morador();
-        $morador->cpf =  sha1($request->input('cpf'));
-        $morador->data_nascimento = $request->input('data_nascimento');
-        $morador->estado_civil = $request->input('estado_civil');
-        $morador->raca = $request->input('raca');
-        $morador->bairro_comunidade = $request->input('bairro_comunidade');
-        
-        
-        
-        if( $morador->save() ){
+            $morador = Morador::where('cpf', sha1($request->input('cpf')))->first();
             $morador->comunidade->projetos;
-            return response()->json($morador, 200);
+            return response()->json(["morador" => $morador, "message" => "CPF já cadastrado"], 200);
+        }else{
+            $morador->cpf =  sha1($request->input('cpf'));
+            $morador->data_nascimento = $request->input('data_nascimento');
+            $morador->estado_civil = $request->input('estado_civil');
+            $morador->raca = $request->input('raca');
+            $morador->bairro_comunidade = $request->input('bairro_comunidade');
+            
+            if( $morador->save() ){
+                $morador->comunidade->projetos;
+                return response()->json(["morador" => $morador, "message" => "Cadastro realizado com sucesso"], 200);
+            }else{
+                return response()->json(["message" => "Não foi possível cadastrar o morador."], 404);
+            }
         }
-      }
-
-    
-    public function show($id)
-    {
-        //
+        
     }
-    
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
 }
